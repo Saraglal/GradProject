c5291f27c3fbe17bc30614f063289d1f9802a99f
MoatazEl-Naggar <65637998+MoatazEl-Naggar@users.noreply.github.com>
2023-04-20 06:59:14 +0200

    Project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+import Login from './Components/login/login'
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        <header>
+            <Login/>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
