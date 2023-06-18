6f9a4e8e06c92dcf59a3e3319e3ec091e6108f73
MoatazEl-Naggar <65637998+MoatazEl-Naggar@users.noreply.github.com>
2023-06-18 17:20:01 +0300

    Project

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,8 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('BloodBankSystem', 'root', '123456789', {
-    host: 'http://bloodbank.mywire.org',
+const sequelize = new Sequelize('bloodbanksystem', 'root1', '123456789Me', {
+    host: 'bloodbanksystem.mysql.database.azure.com',
     dialect: 'mysql',
     port:'3306',
 });
