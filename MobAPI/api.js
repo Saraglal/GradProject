200cb6cbbcf4b3438044548ecb3223ef59114da2
MoatazEl-Naggar <65637998+MoatazEl-Naggar@users.noreply.github.com>
2023-06-23 03:29:35 +0300

    add request transaction

--- MobAPI/api.js
+++ MobAPI/api.js
@@ -9,12 +9,18 @@ const resetPasswordRouter = require('./auth/reset-password');
 
 const profileRouter = require('./profile/profile');
 
+const transactionsRouter = require('./transactions/Transactions');
+const getTransactionsRouter = require('./transactions/getTransactions');
+
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/forgot-password', forgotPasswordRouter);
 app.use('/reset-password', resetPasswordRouter);
 
 app.use('/profile', profileRouter);
+
+app.use('/transactions', transactionsRouter);
+app.use('/getTransactions', getTransactionsRouter);
 
 app.listen(3003, () => {
   console.log('Server listening on port 3003');
