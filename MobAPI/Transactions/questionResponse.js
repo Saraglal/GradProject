f3a2047f4221fabfec3c644e8d7c357a441931e4
MoatazEl-Naggar <65637998+MoatazEl-Naggar@users.noreply.github.com>
2023-07-01 05:12:17 +0300

    MobAPI latest update

--- MobAPI/Transactions/questionResponse.js
+++ MobAPI/Transactions/questionResponse.js
@@ -9,18 +9,20 @@ router.post('/', (req, res) => {
         Q4,
         Q5,
         Q6,
-        Q7
+        Q7,
+        Q8,
     } = req.query;
 
     // Define an array of questions and their corresponding responses
     const questions = [
-        { question: Q1, response: 'Answered yes for Q1.' },
-        { question: Q2, response: 'Answered yes for Q2.' },
-        { question: Q3, response: 'Answered yes for Q3.' },
-        { question: Q4, response: 'Answered yes for Q4.' },
-        { question: Q5, response: 'Answered yes for Q5.' },
-        { question: Q6, response: 'Answered yes for Q6.' },
-        { question: Q7, response: 'Answered yes for Q7.' },
+        { question: Q1, response: 'You must wait for pass 48 hours after you taking  last course of antibiotics to be able to donate.' },
+        { question: Q2, response: 'You had a fever a since a short time ago, you must wait until the end of 3 weeks to be able to donate.' },
+        { question: Q3, response: 'You must. wait for pass 72 hours after taking the last dose of medication in order to be able to donate.' },
+        { question: Q4, response: 'Patients who are on insulin or diabetes medications cannot donate blood.' },
+        { question: Q5, response: 'Chronic disease patients cannot donate.' },
+        { question: Q6, response: 'The last time you donated blood was a short while ago, after 3 months have passerd, you can donate.' },
+        { question: Q7, response: 'Pregnant or breastfeeding women cannot donate.' },
+        { question: Q8, response: 'You must be over 18 to be able to donate.' },
     ];
 
     // Find the first question with an answer of 'yes'
